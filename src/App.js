808dd8f38b77ef900b99c4ea755ce551c736471f
Julia <juliabrain2020@u.northwestern.edu>
2019-01-10 09:46:31 -0600

    part of 3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 //import './App.scss';
 //import('./static/data/products.json').then(json => ...)
 
@@ -6,58 +7,89 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-      <GithubCorner />
-      <main>
-      <Filter />
-      <Shelf />
-      </main>
-      <FloatCart />
+        <GithubCorner />
+        <main>
+          <Filter />
+          <Shelf />
+        </main>
+        <FloatCart />
       </React.Fragment>
-      );
-    }
+    );
   }
+}
 
- 
-  class Filter extends Component {
-  
-    render() {
-      return (
+const availableSizes = ['XS', 'S', 'M', 'ML', 'L', 'XL', 'XXL'];
+class Checkbox extends Component {
+  render() {
+    const { label, classes } = this.props;
+
+    return (
+      <div className={classes}>
+        <label>
+          <input
+            type="checkbox"
+            value={label}
+          />
+
+          <span className="checkmark">{label}</span>
+        </label>
+      </div>
+    );
+  }
+}
+class Filter extends Component {
+  static propTypes = {
+    updateFilters: PropTypes.func.isRequired,
+    filters: PropTypes.array
+  };
+  createCheckbox = label => (
+    <Checkbox
+      classes="filters-available-size"
+      label={label}
+      key={label}
+    />
+  );
+
+  createCheckboxes = () => availableSizes.map(this.createCheckbox);
+  render() {
+    return (
       <div className="filters">
-      <h4 className="title">Sizes:</h4>
+        <h4 className="title">Sizes:</h4>
+        {this.createCheckboxes()}
       </div>
-      );
-    }
+    );
   }
+}
 
 
- 
 
-  class FloatCart extends Component{
-    render(){
-      return(
+
+class FloatCart extends Component {
+  render() {
+    return (
       <button className="square">
-    </button>
-      )
-    }
+      </button>
+    )
   }
+}
 
-  class Shelf extends Component {
-    render(){
-      return (
+class Shelf extends Component {
+  render() {
+    return (
       <div>
-      <div className="header"> </div>
+        <div className="header"> </div>
       </div>
-      )
-    }
+    )
   }
+}
 
-  class GithubCorner extends Component{
-    render(){
-      return(
+class GithubCorner extends Component {
+  render() {
+    return (
       <button className="square">
-    </button>
-      )
-    }
+      </button>
+    )
   }
+}
 
-  export default App;
+export default App;
